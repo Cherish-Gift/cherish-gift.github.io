e91ac714b01776ee6c983ae9ba6e7482869a01d6
hyunsung1123 <gustjd11223@naver.com>
2023-03-07 22:36:21 +0900

    feat:#Navigator 추가

---
+++ src/components/BottomNav.jsx
@@ -0,0 +1,114 @@
+import React,{useRef, useState, useEffect } from "react";
+import "./FontAwesome";
+import "../BottomNav.css";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import throttle from "lodash/throttle";
+
+const BottomNav = () => {
+    // 현재 선택된 아이콘을 관리하는 state
+    const [activeNav, setActiveNav] = React.useState(0);
+    // 스크롤 감지를 관리하는 state
+    const [hide, setHide] = useState(false);
+    const [pageY, setPageY] = useState(0);
+    const documentRef = useRef(document);
+
+    // 이전 y값과 현재 y값을 빼서 state를 변경시켜주는 로직.
+    const handleScroll = () => {
+        const { pageYOffset } = window;
+        const deltaY = pageYOffset - pageY;
+        const hide = pageYOffset !== 0 && deltaY >= 0;
+        setHide(hide);
+        setPageY(pageYOffset);
+        console.log(hide)
+        console.log(pageYOffset);
+        
+    };
+
+    const throttleScroll = throttle(handleScroll, 50);
+    useEffect(() => {
+        documentRef.current.addEventListener("scroll", throttleScroll);
+        return () =>
+            documentRef.current.removeEventListener("scroll", throttleScroll);
+    }, [pageY]);
+
+    return (
+      <nav className={hide ? "wrapper-hidden" : "wrapper"}>
+        {/* 하단 네비게이션 최상위 태그 */}
+        <Link
+          to="/first"
+          className="nav-link"
+          onClick={() => {
+            console.log("render");
+            setActiveNav(1);
+          }}
+        >
+          <div>
+            <FontAwesomeIcon
+              icon="home"
+              className={activeNav === 1 ? "nav-item active" : "nav-item"}
+            />
+            <div className={activeNav === 1 ? "nav-title active" : "nav-title"}>
+              홈
+            </div>
+            {/* 네비게이션을 구성하고 있는 하나의 버튼 */}
+          </div>
+        </Link>
+        <Link
+          to="/second"
+          className="nav-link"
+          onClick={() => {
+            console.log("render");
+            setActiveNav(2);
+          }}
+        >
+          <div>
+            <FontAwesomeIcon
+              icon="compass"
+              className={activeNav === 2 ? "nav-item active" : "nav-item"}
+            />
+            <div className={activeNav === 2 ? "nav-title active" : "nav-title"}>
+              큐레이션
+            </div>
+          </div>
+        </Link>
+        <Link
+          to="/third"
+          className="nav-link"
+          onClick={() => {
+            setActiveNav(3);
+          }}
+        >
+          <div>
+            <FontAwesomeIcon
+              icon="plus"
+              className={activeNav === 3 ? "nav-item active" : "nav-item"}
+            />
+            <div className={activeNav === 3 ? "nav-title active" : "nav-title"}>
+              카테고리
+            </div>
+          </div>
+        </Link>
+        <Link
+          to="/fourth"
+          className="nav-link"
+          onClick={() => {
+            console.log("render");
+            setActiveNav(4);
+          }}
+        >
+          <div>
+            <FontAwesomeIcon
+              icon="medal"
+              className={activeNav === 4 ? "nav-item active" : "nav-item"}
+            />
+            <div className={activeNav === 4 ? "nav-title active" : "nav-title"}>
+              저장목록
+            </div>
+          </div>
+        </Link>
+      </nav>
+    );
+};
+
+export default BottomNav;
---
+++ src/pages/Main.jsx
@@ -0,0 +1,6 @@
+const Main = () => {
+    return (
+      <h1>Main Page 입니다.</h1>  
+    );
+}
+export default Main;
