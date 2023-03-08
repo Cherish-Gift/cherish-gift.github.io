0d407eda88870792e4ffe9dbc93a71c59a436be4
hyunsung1123 <gustjd11223@naver.com>
2023-03-08 10:48:14 +0900

    "feat:#1 수정사항 반영"

--- src/components/FontAwesome.jsx
+++ src/components/FontAwesome.jsx
@@ -1,6 +1,4 @@
-// 라이브러리
 import { library } from "@fortawesome/fontawesome-svg-core";
-// 사용할 아이콘 import
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { faMedal } from "@fortawesome/free-solid-svg-icons";
