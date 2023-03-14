f3d75d45ac9f09db64b8ef44ea5bde3af8c0ae9c
hyunsung1123 <gustjd11223@naver.com>
2023-03-14 21:11:33 +0900

    버튼 컴포넌트 제작

---
+++ src/components/Button.tsx
@@ -0,0 +1,61 @@
+import React, {ReactNode,ButtonHTMLAttributes, CSSProperties } from 'react';
+import axios from "axios";
+
+//ButtonProps는 ButtonHTMLAttributes<HTMLButtonElement> 상속받았기 때문에 
+// button 태그에 대한 모든 속성을 props 사용 가능. 이외의 속성은 따로 선언.
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  children: ReactNode;
+  onClickAction?: () => void; // 버튼 클릭 시 실행될 함수
+  to?: string; // 버튼 클릭 시 이동할 페이지 주소
+  fetchUrl?: string; // 버튼 클릭 시 axios로 요청을 보낼 URL
+  fetchConfig?: object; // axios 요청에 대한 설정(예: method, headers 등)
+  onSuccess?: (response: any) => void; // axios 요청 성공 시 실행될 함수
+  onError?: (error: any) => void; // axios 요청 실패 시 실행될 함수
+  style?: CSSProperties; //button의 style지정.
+};
+
+const Button = ({
+  children,
+  onClickAction,
+  to,
+  fetchUrl,
+  fetchConfig,
+  onSuccess,
+  onError,
+  style,
+  ...rest
+}) => {
+
+  const handleClick = () => {
+    if (onClickAction) {
+      onClickAction();
+    }
+    if (to) {
+      window.location.href = to;
+    }
+    if (fetchUrl) {
+      axios(fetchUrl, fetchConfig)
+        .then((response) => {
+          if (onSuccess) {
+            onSuccess(response);
+          }
+        })
+        .catch((error) => {
+          if (onError) {
+            onError(error);
+          }
+        });
+    }
+  };
+
+  return (
+    <>
+      <button {...rest} onClick={handleClick} style={style}>
+        {children}
+      </button>
+    </>
+  );
+};
+
+export default Button;
--- src/pages/First.tsx
+++ src/pages/First.tsx
@@ -1,103 +1,128 @@
-const first =() => {
-    return (
-      <div>
-        <h1>첫번째 화면입니다.</h1>
-        <h1>검색창</h1>
-        <h1>상품사진</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-        <h1>First</h1>
-      </div>
-    );
-}
+import Button from "../components/Button";
+import "../assets/Button.scss"
+const first = () => {
+
+  const handleButtonClick = () => {
+    console.log("Button Clicked!");
+  };
+  const handleFetchSuccess = (response: any) => {
+    console.log("Fetch Success!", response.data);
+  };
+
+  const handleFetchError = (error: any) => {
+    console.error("Fetch Error!", error);
+  };
+  return (
+    <div>
+      <h1>첫번째 화면입니다.</h1>
+      <h1>검색창</h1>
+      <h1>상품사진</h1>
+      <h1>First</h1>
+      <Button
+        children="다음"
+        onClickAction={handleButtonClick}
+        className="deactivate"
+      />
+    
+      <Button children="다음" to="/first" className="activate" />
+ 
+      <Button children="다음" type="submit" className="default" />
+
+      <Button
+        children="다음"
+        onClickAction={handleButtonClick}
+        fetchUrl="https://jsonplaceholder.typicode.com/todos/"
+        onSuccess={handleFetchSuccess}
+        onError={handleFetchError}
+        className="default2"
+      />
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+      <h1>First</h1>
+    </div>
+  );
+};
 export default first;
